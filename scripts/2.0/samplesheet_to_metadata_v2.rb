5835e7a13494e697e505e75fd01b42547b876e46
Marc Hoeppner <mphoeppner@gmail.com>
2023-10-27 10:24:54 +0200

    Adding fix for overly precise date stamps

--- scripts/2.0/samplesheet_to_metadata_v2.rb
+++ scripts/2.0/samplesheet_to_metadata_v2.rb
@@ -84,20 +84,27 @@ opts.on("-h","--help","Display the usage information") {
 opts.parse! 
 
 # Report any keys that were not defined in the reference sheet
+# This can be used to sync up the samplesheet with the actual standard afterwards
 undefined_keys = []
 
+###################################
 # Check for reference metadata info
-metadata_reference = File.dirname(__FILE__) + "/../../metadata/2.0/CRC1182_NGS_data_v2.txt"
+###################################
 
+metadata_reference = File.dirname(__FILE__) + "/../../metadata/2.0/CRC1182_NGS_data_v2.txt"
 raise "Could not find the reference metadata sheet" unless File.exist?(metadata_reference)
 
 ref_keys = {}
+
 IO.readlines(metadata_reference).each do |line|
     e,s = line.strip.split("\t")
     ref_keys[e] = s unless ref_keys.has_key?(e)
 end
 
+#######################
 # Parse XLS samplesheet
+#######################
+
 ### Convert XLSX to CSV
 xlsx = RubyXL::Parser.parse(options.infile)
 
@@ -112,7 +119,9 @@ main_contact_email = nil
 principle_investigator = nil
 
 front[1..5].each do |row|
-
+    
+    next if row.nil?
+        
     key = row[0].value
 
     row[1] ? val = row[1].value : val = nil
@@ -142,7 +151,7 @@ meta.sheet_data[0][0..ref_keys.keys.length].each_with_index do |u,i|
     units << u.value unless u.value.nil?
 end
 
-# Get the column  headers
+# Get the column headers for later use
 header = []
 meta.sheet_data[1][0..ref_keys.keys.length].each_with_index do |h,i|
     header << h.value.gsub(" ", "_") unless h.value.nil?
@@ -150,22 +159,28 @@ end
 
 # Iterate over each row using the column headers to extract annotations
 # We assume no more than 400 rows, which is usually reasonable
+# This starts at row 3, as 1+2 are headers 
 
 meta.sheet_data[2..400].each_with_index do |r,idx|
 
     data = {}
     header.each_with_index do |h,i|
 
+	# Deal with empty columns without having the parser blow up
         r[i] ? val = r[i].value.to_s : val = nil
 
         if val && val.length > 0
+            if val.include?("T00:00")
+                val = val.split("T00:00")[0]
+            end
             data[h] = val
         else
             warn "Missing mandatory value for #{h}" if ref_keys[h] == 1
         end
 
     end
-
+    
+    # Write the iRODS metadata package
     f = File.new(data["library_id"] + ".meta", "w+")
 
     f.puts "crc_project\t#{project_id}\tstring"
@@ -182,4 +197,5 @@ meta.sheet_data[2..400].each_with_index do |r,idx|
     end
 
     f.close
+    
 end
